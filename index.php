73b7d3e291bf95099a77e439dbf43396019dd257
francesca-campana <franx3rox@gmail.com>
2020-07-16 16:51:39 +0200

    start

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include __DIR__ . 'database.php'; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dist/app.css">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
